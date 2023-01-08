53aebdbcfa0a9f026f8a11b7b79fcb5ded46e130
kamchatka-volcano <gorelyy@vivaldi.net>
2023-01-08 14:31:07 +0300

    -added AccessPermission utility for implementing a limited friend access to private methods (the badge pattern)

--- include/sfun/interface.h
+++ include/sfun/interface.h
@@ -10,7 +10,8 @@ class Interface {
 public:
     Interface()
     {
-        static_assert(std::is_base_of_v<Interface<T>, T> && !std::is_convertible_v<T*, Interface<T>*>,
+        static_assert(
+                std::is_base_of_v<Interface<T>, T> && !std::is_convertible_v<T*, Interface<T>*>,
                 "T must be derived from Interface<T> privately");
     }
     virtual ~Interface() = default;
@@ -18,6 +19,33 @@ public:
     Interface& operator=(const Interface&) = delete;
     Interface(Interface&&) = delete;
     Interface& operator=(Interface&&) = delete;
+};
+
+template <typename T>
+struct AccessToken {
+private:
+    AccessToken(T&){};
+    AccessToken(){};
+    friend T;
+};
+
+namespace detail {
+
+template <typename T>
+struct AccessPermissionBase {
+    void operator()(AccessToken<T>) {}
+};
+
+} // namespace detail
+
+template <typename... Ts>
+struct AccessPermission : private detail::AccessPermissionBase<Ts>... {
+    template <typename TAccessToken>
+    AccessPermission(TAccessToken accessToken)
+    {
+        operator()(accessToken);
+    }
+    using detail::AccessPermissionBase<Ts>::operator()...;
 };
 
 } // namespace sfun
