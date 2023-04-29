58ec11d76a83ae6c983fa6cf4b453f66e868ee0f
kamchatka-volcano <gorelyy@vivaldi.net>
2023-04-29 15:51:31 +0300

    -updated include guards in contract.h

--- include/sfun/contract.h
+++ include/sfun/contract.h
@@ -1,5 +1,5 @@
-#ifndef STUFF_FROM_UNNAMED_NAMESPACE_CONTRACT_H
-#define STUFF_FROM_UNNAMED_NAMESPACE_CONTRACT_H
+#ifndef STUFF_FROM_UNNAMED_NAMESPACE_V4_AND_NEWER_CONTRACT_H
+#define STUFF_FROM_UNNAMED_NAMESPACE_V4_AND_NEWER_CONTRACT_H
 
 ///
 ///Renamed part of Microsoft.GSL assert header
@@ -8,7 +8,7 @@
 #if defined(__clang__) || defined(__GNUC__)
 #define _sfun_likely(x) __builtin_expect(!!(x), 1)
 #else
-#define _sfunLikely(x) (!!(x))
+#define _sfun_likely(x) (!!(x))
 #endif //defined(__clang__) || defined(__GNUC__)
 
 #define sfun_contract_check(cond) (_sfun_likely(cond) ? static_cast<void>(0) : std::terminate())
@@ -17,4 +17,4 @@
 #define sfun_postcondition sfun_contract_check
 #define sfun_invariant sfun_contract_check
 
-#endif //STUFF_FROM_UNNAMED_NAMESPACE_CONTRACT_H
+#endif //STUFF_FROM_UNNAMED_NAMESPACE_V4_AND_NEWER_CONTRACT_H
